c372df32fd58006cdbe9a964a244990689561efe
Yaroslav Fedorchuk <yafedorchuck@3pm.ai>
2022-11-02 19:41:12 +0500

    finish test create handler

--- internal/app/http/handler_test.go
+++ internal/app/http/handler_test.go
@@ -1,31 +1,34 @@
 package http
 
 import (
-	"context"
 	mock_http "github.com/SpiridonovDaniil/Distributed-config/internal/app/http/mocks"
-	"github.com/gofiber/fiber/v2"
+	"github.com/SpiridonovDaniil/Distributed-config/internal/domain"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"strings"
 	"testing"
 )
 
 func TestHandler_createHandler(t *testing.T) {
-	type mockBehavior func(s *mock_http.Mockservice, ctx context.Context)
+	type mockBehavior func(s *mock_http.Mockservice, expectedError error)
 	testTable := []struct {
-		name string
-		inputBody string
-		inputRequest context.Context
-		mockBehavior mockBehavior
+		name                   string
+		inputBody              string
+		mockBehavior           mockBehavior
 		expectedTestStatusCode int
-		expectRequest error
-	} {
+		expectedError          error
+		expectedResponse       string
+	}{
 		{
-			name: "OK",
-			inputBody: `{"service":"test", "data":"{"key1":"test1"}, {"key2":"test2"}"}`,
-			mockBehavior: func(s *mock_http.Mockservice, ctx context.Context) {
-				s.EXPECT().Create(ctx, ctx).Return(nil)
+			name:      "get HTTP status 201",
+			inputBody: `{"service":"test","data":[{"key1":"test1"},{"key2":"test2"}]}`,
+			mockBehavior: func(s *mock_http.Mockservice, expectedError error) {
+				s.EXPECT().Create(gomock.Any(), gomock.AssignableToTypeOf(&domain.Request{})).Return(expectedError)
 			},
-			expectedTestStatusCode: 200,
-			expectRequest: nil,
+			expectedTestStatusCode: 201,
+			expectedResponse:       "",
 		},
 	}
 
@@ -35,10 +38,63 @@ func TestHandler_createHandler(t *testing.T) {
 			defer c.Finish()
 
 			create := mock_http.NewMockservice(c)
-			testCase.mockBehavior(create, testCase.inputRequest)
+			testCase.mockBehavior(create, testCase.expectedError)
 
-			f := fiber.New()
+			f := NewServer(create)
+			req, err := http.NewRequest("POST", "/config", strings.NewReader(testCase.inputBody))
+			req.Header.Add("content-Type", "application/json")
+			assert.NoError(t, err)
 
+			resp, err := f.Test(req)
+			assert.NoError(t, err)
+
+			body, err := io.ReadAll(resp.Body)
+			assert.Equal(t, testCase.expectedTestStatusCode, resp.StatusCode)
+			assert.Equal(t, testCase.expectedResponse, string(body))
+		})
+	}
+}
+
+func TestHandler_rollBackHandler(t *testing.T) {
+	type mockBehavior func(s *mock_http.Mockservice, expectedError error)
+	testTable := []struct {
+		name                   string
+		inputBody              string
+		mockBehavior           mockBehavior
+		expectedTestStatusCode int
+		expectedError          error
+		expectedResponse       string
+	}{
+		{
+			name:      "get HTTP status 200",
+			inputBody: `{"service":"test","data":[{"key1":"test1"},{"key2":"test2"}]}`,
+			mockBehavior: func(s *mock_http.Mockservice, expectedError error) {
+				s.EXPECT().Create(gomock.Any(), gomock.AssignableToTypeOf(&domain.Request{})).Return(expectedError)
+			},
+			expectedTestStatusCode: 200,
+			expectedResponse:       "",
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			create := mock_http.NewMockservice(c)
+			testCase.mockBehavior(create, testCase.expectedError)
+
+			f := NewServer(create)
+			req, err := http.NewRequest("POST", "/config", strings.NewReader(testCase.inputBody))
+			req.Header.Add("content-Type", "application/json")
+			assert.NoError(t, err)
+
+			resp, err := f.Test(req)
+			assert.NoError(t, err)
+
+			body, err := io.ReadAll(resp.Body)
+			assert.Equal(t, testCase.expectedTestStatusCode, resp.StatusCode)
+			assert.Equal(t, testCase.expectedResponse, string(body))
 		})
 	}
 }
